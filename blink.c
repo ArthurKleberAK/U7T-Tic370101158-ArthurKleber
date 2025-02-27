35bbc6c6033592b5d69aa285014843a576cd1d4a
Arthur Kleber <arthurkisd@gmail.com>
2025-02-26 21:28:40 -0300

    comentando algumas linhas

--- blink.c
+++ blink.c
@@ -7,32 +7,38 @@
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
-//arquivo .pio
-#include "blink.pio.h"
+#include "blink.pio.h" //arquivo .pio
 
+// DEFINIÇÕES DO PWM:
 #define CLOCK_DIV 64.0 
 #define TPWM 20000.0    
 #define WRAP 39063
 
+// DEFINIÇÕES DO DISPLAY:
 #define I2C_PORT i2c1
 #define I2C_SDA 14 // GPIO SDA DO DISPLAY
 #define I2C_SCL 15 // GPIO SCL DO DISPLAY
 #define endereco 0x3C // ENDEREÇO DO DISPLAY
+ssd1306_t ssd; // INICIALIZA A ESTRUTURA DO DISPLAY
 
-#define NUM_PIXELS 25 //número de LEDs
-#define OUT_PIN 7 // pino de saída da matriz de led 5x5
-#define buzzer 10 // Buzzer para simular o motor acionado por PWM
-#define LED_R 13 // GPIO LED VERMELHO
-#define LED_B 12 // GPIO LED AZUL
-#define LED_G 11 // GPIO LED VERDE
-#define BOTAO_A 05
-#define BOTAO_B 06 // Define o botão b com Start
-#define SW 22
-#define DEBOUNCE 200
-volatile uint32_t ultima_interrup = 0; // Para armazenar o último tempo de interrupção
-volatile bool timer_expired = false; // Flag para indicar se o temporizador expirou
+// DEFINIÇÕES DOS PINOS:
+#define MATRIX_PIN 7 // GPIO DE SAÍDA DA MATRIZ DE LED 5x5
+#define BUZZER 10 // GPIO BUZZER
+#define LED_R 13 // GPIO LED VERMELHO FASE DE AQUECIMENTO
+#define LED_B 12 // GPIO LED AZUL  FASE DE INGREDIENTES
+#define LED_G 11 // GPIO LED VERDE INIDICA QUE O PROGRAMA ESTA FUNCIONANDO
+#define BOTAO_A 05 // GPIO DO BOTÃO A
+#define BOTAO_B 06 // GPIO DO BOTÃO A
+#define SW 22 // GPIO DO JOYSTICK
+#define DEBOUNCE 200 // TEMPO EM MS DO DEBOUCE DE CADA BOTÃO ACIONADO 
 
-// Definição dos estados da máquina de estados
+//NÚMERO DE LEDS DA MATRIZ
+#define NUM_PIXELS 25 
+
+volatile uint32_t ultima_interrup = 0; // PARA ARMAZENAR O ÚLTIMO TEMPO DE CADA INTERRUPÇÃO 
+volatile bool timer_expired = false; // FLAG PARA INDICAR SE O TEMPORIZADO EXPIROU 
+
+// DEFINIÇÃO DOS ESTADOS DA MÁQUINA DE ESTADO
 typedef enum {
     ESPERA,
     ENCHER_ING1,
@@ -42,54 +48,51 @@ typedef enum {
     DRENAR,
     FINALIZAR
 } Estado;
-Estado estado_atual = ESPERA;
 
-
-
-ssd1306_t ssd; // Inicializa a estrutura do display
+// FUNÇÃO RESPONSAVEL POR INICIALIZAR O DISPLAY
 void init_display(){
-    i2c_init(I2C_PORT, 400 * 1000); // Inicialização do I2C utilizando 400Khz
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Define GPIO como I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Define GPIO como I2C
-    gpio_pull_up(I2C_SDA); // Define GPIO SDA como pullup
-    gpio_pull_up(I2C_SCL); // Define GPIO SCL como pullup
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd); // Configura o display
-    ssd1306_fill(&ssd, false); // Limpa o display
+    i2c_init(I2C_PORT, 400 * 1000); // INICIALIZAÇÃO DO I2C UTILIZANDO 400KHZ 
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // DEFINE  GPIO COMO I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // DEFINE  GPIO COMO I2C
+    gpio_pull_up(I2C_SDA); // DEFINE GPIO SDA COMO PULLUP
+    gpio_pull_up(I2C_SCL); // DEFINE GPIO SCL COMO PULLUP
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // INICIALIZAR O DISPLAY
+    ssd1306_config(&ssd); // CONFIGURA O DISPLAY
+    ssd1306_fill(&ssd, false); // LIMPA O DISPLAY
 }
 
 
-// Função de callback do temporizador
+// FUNÇÃO DE CALLBACK DO TEMPORIZADOR 
 bool repeating_timer_callback(struct repeating_timer *t) {
-    timer_expired = true; // Define a flag para indicar que o temporizador expirou
-    return true; // Retorna true para continuar chamando a função
+    timer_expired = true; // DEFINE A FLAG PARA INDICAR QUE O TEMPORIZADOR EXPIROU 
+    return true; //  RETORNA VERDADEIRO PARA CONTINUAR CHAMANDO A FUNÇÃO 
 }
 
+// FUNÇÃO RESPONSAVEL POR DESENHAR NA MATRIZ DE LED:
 int MAPPER[25] = {
     24, 23, 22, 21, 20,
-    15, 16, 17, 18, 19,
+    15, 16, 17, 18, 19,    // MAPA DE CADA LED NA BITDOGLAB
     14, 13, 12, 11, 10,
     5, 6, 7, 8, 9,
     4, 3, 2, 1, 0};
 
 typedef struct {
-   volatile uint8_t r; // Red
-    volatile uint8_t g; // Green
-   volatile uint8_t b; // Blue
-
+    volatile uint8_t r; // RED
+    volatile uint8_t g; // GREEN
+    volatile uint8_t b; // BLUE
 } Cor;
+
 uint32_t matrix_rgb1(Cor cor) {
     return (cor.r << 24) | (cor.b << 16) | (cor.g << 8);
 }
-// int16_t i = NUM_PIXELS - 1; i >= 0; i--
+
 void desenho_pio1(Cor *desenho, uint32_t valor_led, PIO pio, uint sm) {
-    for (int16_t i = 0; i < NUM_PIXELS; i++) { // Acessa de trás para frente
+    for (int16_t i = 0; i < NUM_PIXELS; i++) { 
         int led_matrix_location = MAPPER[i];
         valor_led = matrix_rgb1(desenho[led_matrix_location]);
         pio_sm_put_blocking(pio, sm, valor_led);
     }
 }
-
 
 Cor matriz_f[25] = {
 {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
@@ -98,8 +101,6 @@ Cor matriz_f[25] = {
 {0, 0, 0}, {10, 0, 40}, {0, 0, 40}, {10, 0, 40}, {0, 0, 0},
 {10, 0, 40}, {0, 0, 40}, {0, 0, 40}, {0, 0, 40}, {10, 0, 40}
 };
-
-
 Cor matriz_0[25] = {
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},  
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -277,7 +278,6 @@ Cor matriz_21[25] =
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, 
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_22[25] =
 {
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -286,7 +286,6 @@ Cor matriz_22[25] =
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, 
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_23[25] =
 {
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -295,7 +294,6 @@ Cor matriz_23[25] =
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, 
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_24[25] =
 {
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -304,7 +302,6 @@ Cor matriz_24[25] =
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
 {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_25[25] =
 {
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -321,7 +318,6 @@ Cor matriz_26[25] =
 {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
 {0,0,0}, {1,1,6}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_27[25] =
 {
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -330,7 +326,6 @@ Cor matriz_27[25] =
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
     {0,0,0}, {0,0,0}, {1,1,6}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_28[25] =
 {
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -339,7 +334,6 @@ Cor matriz_28[25] =
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
     {0,0,0}, {0,0,0}, {0,0,0}, {1,1,6}, {1,1,6}
 };
-
 Cor matriz_29[25] =
 {
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, 
@@ -349,36 +343,35 @@ Cor matriz_29[25] =
     {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,1,6}
 };
 
+// PONTEIRO PARA ARMAZENAR A MATRIZ ATIVA:
+Cor* matrizes[29] = {
+matriz_1, matriz_2, matriz_3, matriz_4, matriz_5, matriz_6, matriz_7, matriz_8, matriz_9, matriz_10, matriz_11,
+matriz_12, matriz_13, matriz_14, matriz_15, matriz_16, matriz_17, matriz_18, matriz_19, matriz_20, 
+matriz_21, matriz_22, matriz_23, matriz_24, matriz_25, matriz_26, matriz_27, matriz_28, matriz_29
+};
 
-    // Ponteiro para armazenar a matriz ativa
-    Cor* matrizes[29] = {
-    matriz_1, matriz_2, matriz_3, matriz_4, matriz_5, matriz_6, matriz_7, matriz_8, matriz_9, matriz_10, matriz_11,
-    matriz_12, matriz_13, matriz_14, matriz_15, matriz_16, matriz_17, matriz_18, matriz_19, matriz_20, 
-    matriz_21, matriz_22, matriz_23, matriz_24, matriz_25, matriz_26, matriz_27, matriz_28, matriz_29
-    };
+// FUNÇÃO DE TEMPORIZADOR PARA CRIAR UM ATRASO EM CADA ANIMAÇÃO:
+void loop_com_timer_nao_bloqueante(PIO pio,  const uint8_t matrizes[][8], uint valor_led) {
+    uint64_t tempo_anterior = time_us_64(); // CAPTURA O TEMPO INICIAL
+    int j = 0;} 
+void loop_com_timer(PIO pio,  const uint8_t matrizes[][8], uint valor_led) {
+    uint64_t tempo_anterior = time_us_64(); // CAPTURA O TEMPO INICIAL
+    int k = 10;}
 
 
-    void loop_com_timer_nao_bloqueante(PIO pio,  const uint8_t matrizes[][8], uint valor_led) {
-        uint64_t tempo_anterior = time_us_64(); // Captura o tempo inicial
-        int j = 0;}
-    
-    void loop_com_timer(PIO pio,  const uint8_t matrizes[][8], uint valor_led) {
-            uint64_t tempo_anterior = time_us_64(); // Captura o tempo inicial
-            int k = 10;}
-
-// Estrutura para armazenar o estado do PWM
-typedef struct {
-    uint slice_num;   // Número do slice PWM associado ao pino.
-    bool pwm_active;  // Indica se o PWM está ativo ou não.
+// DEFINIÇÕES DO PWM:
+typedef struct { // ESTRUTURA PARA ARMAZENAR O ESTADO DO PWM
+    uint slice_num;   // NÚMERO DO SLICE PWM ASSOCIADO PINO
+    bool pwm_active;  // INDICA SE O PWM ESTÁ ATIVO OU NÃO
 } pwm_state_t;
 
-// Função de callback do temporizador
+// FUNÇÃO DE NTERRUPÇÃO DO TEMPORIZADOR DO PWM 
 bool pwm_timer_callback(repeating_timer_t *rt) {
-    pwm_state_t *state = (pwm_state_t *)rt->user_data; // Recupera o estado do PWM armazenado em user_data.
+    pwm_state_t *state = (pwm_state_t *)rt->user_data; // RECRUTA O ESTADO DO PWM ARMAZENADO EM user_data.
 
-    if (state->pwm_active) {          // Verifica se o PWM está ativo.
-        pwm_set_enabled(state->slice_num, false); // Desativa o PWM no slice correspondente.
-        state->pwm_active = false;    // Atualiza o estado para indicar que o PWM não está mais ativo.
+    if (state->pwm_active) {          // VERIFICA SE O PWM ESTÁ ATIVO
+        pwm_set_enabled(state->slice_num, false); // DESATICA O PWM NO SLICE CORESPONDENTE 
+        state->pwm_active = false;    //  ATUALIZA O ESTADO PARA INDICAR QUE O PWM NÃO ESTÁ MAIS ATIVO .
         return false;                 // Para o temporizador, pois o PWM foi desativado.
     }
     return true;                      // Retorna true para continuar o temporizador se o PWM não estava ativo.
@@ -442,7 +435,7 @@ int main()
     double r , g , b ;
     uint offset = pio_add_program(pio, &blink_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    blink_program_init(pio, sm, offset, OUT_PIN);
+    blink_program_init(pio, sm, offset, MATRIX_PIN);
     uint64_t tempo_anterior = time_us_64(); // Captura o tempo inicial
    // variaveis utilizadas na estrutura de repetição de cada animação da matriz:
     volatile int j = 0;
@@ -461,9 +454,8 @@ int main()
         switch (estado_atual) {
             
             case ESPERA:
-            desenho_pio1(matriz_0, valor_led, pio, sm); // Limpa a matriz de Led
-            
-            //gpio_put(LED_G, 0);  // LEMBRAR DE ALTERAAAAAAAAAAAAAAAAAAAAA PARA 1
+            desenho_pio1(matriz_0, valor_led, pio, sm); // Limpa a matriz de Led        
+            gpio_put(LED_G, 1);  // INDICA QUE O PROGRAMA ESTA ATIVO 
             ssd1306_draw_string(&ssd, "Sistema ON ", 8, 10); 
             ssd1306_draw_string(&ssd, " ABERTE ", 8, 30); 
             ssd1306_draw_string(&ssd, "O BOTAO B ", 8, 40 );
@@ -481,9 +473,9 @@ int main()
             break;
             
             case ENCHER_ING1:
-           // gpio_put(LED_G, 0);   
+            gpio_put(LED_G, 0);   
            
-            gpio_put(LED_B, 0);   // LEMBRAR DE ALTERAAAAAAAAAAAAAAAAAAAAA PARA 1            
+            gpio_put(LED_B, 1); // INDICA QUE O PROGRAMA ESTÁ NA FASE DE RECEBER OS INGREDIENTES           
             while (j < 10) { 
                 uint64_t tempo_atual = time_us_64();       
                 if (tempo_atual - tempo_anterior >= 1000000) { // Se 1 segundo se passou
@@ -553,9 +545,9 @@ int main()
             ssd1306_send_data(&ssd); // Atualiza o display              
             desenho_pio1(matriz_f, valor_led, pio, sm);
             gpio_put(LED_B, 0);
-           
-            // Chama a função buzz com o pino do buzzer e as durações desejadas
-            pwm_control(buzzer,5000);
+            gpio_put(LED_R, 1);
+            // Chama a função buzz com o pino do BUZZER e as durações desejadas
+            pwm_control(BUZZER,5000);
                                      
             if (tempo_estado >= 5000) { // 10 segundos  
                 ssd1306_fill(&ssd, false); // false para limpar (preencher com preto)
@@ -570,6 +562,7 @@ int main()
             ssd1306_draw_string(&ssd, "PROCESSO ", 8, 10); 
             ssd1306_draw_string(&ssd, " FINALIZADO ", 8, 30); 
             ssd1306_send_data(&ssd); // Atualiza o display  
+            gpio_put(LED_R, 0);
             
             if (tempo_estado >= 5000) { // 10 segundos  
                 ssd1306_fill(&ssd, false); // false para limpar (preencher com preto)
@@ -595,9 +588,6 @@ int main()
             desenho_pio1(matriz_0, valor_led, pio, sm);
             
             if (tempo_estado >= 2000) { // 2 segundos 
-               // gpio_put(LED_R, 0);// LEMBRAR DE ALTERAAAAAAAAAAAAAAAAAAAAA PARA 1
-               // gpio_put(LED_G, 0);// LEMBRAR DE ALTERAAAAAAAAAAAAAAAAAAAAA PARA 1
-               // gpio_put(LED_B, 0);  // LEMBRAR DE ALTERAAAAAAAAAAAAAAAAAAAAA PARA 1
                 ssd1306_fill(&ssd, false); // false para limpar (preencher com preto)
                 ssd1306_send_data(&ssd); // Atualiza o display            
                 estado_atual = FINALIZAR;
@@ -609,9 +599,6 @@ int main()
             ssd1306_draw_string(&ssd, "FIM", 8, 30); 
             ssd1306_send_data(&ssd); // Atualiza o display 
             if (tempo_estado >= 3000) { // 2  
-           // gpio_put(LED_R, 0);
-           // gpio_put(LED_G, 0);
-            // gpio_put(LED_B, 0);  
             ssd1306_fill(&ssd, false); // false para limpar (preencher com preto)
             ssd1306_send_data(&ssd); // Atualiza o display 
             estado_atual = ESPERA;
@@ -619,7 +606,6 @@ int main()
             }
                 break;
         }
-        sleep_ms(100);
     }
 }
 
